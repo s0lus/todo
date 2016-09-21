28ea8461eac7566ffc02a44da3ea83c2753ffc89
s0lus <mashenkoalex@gmail.com>
2016-09-21 21:07:55 +0900

    Now we can create new ToDo

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -2,10 +2,16 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Todo;
+use AppBundle\Entity\ToDo;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class TodoController extends Controller
 {
@@ -23,7 +29,45 @@ class TodoController extends Controller
      */
     public function createAction(Request $request)
     {
-        return $this->render('todo/create.html.twig');
+        $todo = new ToDo();
+
+        $form = $this->createFormBuilder($todo)
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('category', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('priority', ChoiceType::class, array('choices' => array('Low' => 'Low', 'Medium' => 'Medium', 'High' => 'High'), 'attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('due_date', DateTimeType::class, array('attr' => array('class' => 'formcontrol', 'style' => 'margin-bottom:15px')))
+            ->add('Save', SubmitType::class, array('label' => 'Create ToDo', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-bottom:15px')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $name = $form['name']->getData();
+            $category = $form['category']->getData();
+            $description = $form['description']->getData();
+            $priority = $form['priority']->getData();
+            $due_date = $form['due_date']->getData();
+
+            $now = new \DateTime('now');
+
+            $todo->setName($name);
+            $todo->setCategory($category);
+            $todo->setDescription($description);
+            $todo->setPriority($priority);
+            $todo->setDueDate($due_date);
+            $todo->setCreateDate($now);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($todo);
+            $em->flush();
+
+            $this->addFlash('Notice', 'ToDo added');
+
+            return $this->redirectToRoute('todo_list');
+        }
+
+        return $this->render('todo/create.html.twig', array('form' => $form->createView()));
     }
 
     /**
